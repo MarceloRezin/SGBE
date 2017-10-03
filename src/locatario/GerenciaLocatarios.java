699eacf40ee127071b45cd6e67e9613f27ef5f96
Usuario <Usuario@Usuario-PC>
2017-10-03 08:23:23 -0300

    Reescrita do for e inserção do teste da lista vazia.

--- src/locatario/GerenciaLocatarios.java
+++ src/locatario/GerenciaLocatarios.java
@@ -12,11 +12,12 @@ public class GerenciaLocatarios {
 	
 	//Consulta pelo Nome e Retorna o Locador.
 	public Locatario consultaPorNome(String nome) {
-		//Percorre o Array Buscando pelo Nome.
-		for(int i = 0; i < locador.size(); i++) {
-			Locatario l = locador.get(i);
-			if(l.getNomeCompleto() == nome) {
-				return l;
+		if(locador == null) {
+			return null;
+		}
+		for (Locatario locatario : locador){
+			if(locatario.getNomeCompleto() == nome) {
+				return locatario;
 			}
 		}
 		return null;		
@@ -24,12 +25,14 @@ public class GerenciaLocatarios {
 	
 	//Conslta pela Serie e Retorna a Turma.
 	public ArrayList<Locatario> consultaPorSerie(String serie) {
+		if(locador == null) {
+			return null;
+		}
 		ArrayList<Locatario> turma = null;
 		//Percorre o Array Buscando pela Serie e Armazenando em Turma.
-		for(int i = 0; i < locador.size(); i++) {			
-			Locatario l = locador.get(i);
-			if(l.getSerie() == serie) {
-				turma.add(l);
+		for(Locatario locatario : locador) {			
+			if(locatario.getSerie() == serie) {
+				turma.add(locatario);
 			}			
 		}
 		return turma;
@@ -41,7 +44,10 @@ public class GerenciaLocatarios {
 	}
 	
 	//Removo um Locador da Lista.
-	public void removeLocador(Locatario locador) {		
+	public void removeLocador(Locatario locador) {
+		if(locador == null) {
+			return;
+		}
 		this.locador.remove(locador);		
 	}
 		
