202f0697a08ca05dadcabb9c1df2585b81e28cba
MarceloRezin <marcelo.rezin@gmail.com>
2017-10-08 10:15:35 -0300

    Correção do método toString()

--- src/livro/Livro.java
+++ src/livro/Livro.java
@@ -70,10 +70,10 @@ public class Livro {
 			dis = "Indisponivel";
 		}
 		
-		return	"Numero De Registro: " + numeroRegistro + "/n" +
-				"Titulo: " + titulo + "/n" +
-				"Autor: " + autor + "/n" +
-				"Localização: "	+ localizacao + "/n"+ 
+		return	"Numero De Registro: " + numeroRegistro + "\n" +
+				"Titulo: " + titulo + "\n" +
+				"Autor: " + autor + "\n" +
+				"Localização: "	+ localizacao + "\n"+ 
 				"Disponivel: " + dis;
 	}
 	
