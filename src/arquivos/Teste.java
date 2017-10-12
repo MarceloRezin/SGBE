f9d916c3217c36569866463e1ab18037fc7f45dd
MarceloRezin <marcelo.rezin@gmail.com>
2017-10-12 14:50:58 -0300

    Testes Realizados

--- src/arquivos/Teste.java
+++ src/arquivos/Teste.java
@@ -8,17 +8,25 @@ public class Teste {
 
 	public static void main(String[] args) throws Exception {
 		
-		Locatario l = new Locatario("Maelo", "06/08/1998", "ee", "sdsd", "44", "c", "d");
+		
+		Locatario l = new Locatario("Marcelo Rezin", "06/08/1998", "Estrada Geral, Vila maria, Treze de Maio", "sdsd", "44", "c", "d");
+		Locatario l1 = new Locatario("José Antonio", "02/12/1698", "Estrada Geral", "sdsd", "44", "c", "d");
+		Locatario l3 = new Locatario("Marcos", "16/03/1458", "Estrada Geral, Vila maria", "sd sd", "44", "c", "d");
+		
 		
 		ArrayList<Locatario> ls = new ArrayList<>();
 		
 		ls.add(l);
+		ls.add(l1);
+		ls.add(l3);
 		
-		new GravaJSON<>(ls, Diretorio.DIR_LOCATARIOS).call();
+		//new GravaJSON<>(ls, Diretorio.DIR_LOCATARIOS).call();
 		
 		ls = new LeJsonLocatario().call();
 		
-		System.out.println(ls.get(0).toString());
+		for (Locatario locatario : ls) {
+			System.out.println(locatario.toString());
+		}
 		
 	}
 
